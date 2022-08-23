fb64732e8c59ed25e5805488dd7a24b136fa7791
gwu-zymo <47827599+gwu-zymo@users.noreply.github.com>
2022-08-23 12:53:08 -0700

    Update pull_vaginal.py

--- pull_vaginal.py
+++ pull_vaginal.py
@@ -18,7 +18,7 @@ while line:
     run_ID = line_split[1]
     all_run[run_ID] = line_split[0]
     all_ID[line_split[2]] = run_ID
-    all_metadata[line_split[9]] = line.strip('\n').replace(',', '-')
+    all_metadata[line_split[2]] = line.strip('\n').replace(',', '-')
     line = inp.readline()
 inp.close()
 
