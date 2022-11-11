0a6db483b1a39c061b5030af23219c881aa66dde
gwu-zymo <47827599+gwu-zymo@users.noreply.github.com>
2022-11-11 13:00:05 -0800

    Update edit_items.py

--- edit_items.py
+++ edit_items.py
@@ -1,2 +1,13 @@
 import boto3
 
+response = table.update_item(
+    Key={
+        'ReleaseNumber': releaseNumber,
+        'Timestamp': result[0]['Timestamp']
+    },
+    UpdateExpression="set Sanity = :r",
+    ExpressionAttributeValues={
+        ':r': 'false',
+    },
+    ReturnValues="UPDATED_NEW"
+)
