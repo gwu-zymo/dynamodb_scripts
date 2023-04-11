3ce2a6597b1e6a3a5363a9b71407f106081fcd10
gwu-zymo <47827599+gwu-zymo@users.noreply.github.com>
2023-04-11 11:26:37 -0700

    Update modify_TOC.py

--- modify_TOC.py
+++ modify_TOC.py
@@ -4,7 +4,7 @@ float_list = ["ct_V1V3", "ct_ITS", "ct_AMR1", "ct_AMR2", "ct_AMRm", "Age_Years"]
 
 inp = open('TOC_4_10_23.txt', 'r')
 oup = open('TOC_4_10_23_mod.txt', 'w')
-line = inp.readline()
+header = inp.readline()
 oup.write(line)
 lines = inp.readlines()
 lines_merged = []
@@ -16,8 +16,10 @@ for i in range(len(lines)-1, -1, -1):
     lines_merged.append(lines[i].strip('.\n') + tmp_line)
     tmp_line = ''
 
+lines_merged_split = []
 for line in lines_merged:
   split_line = line.split('\t')
+  lines_merged_split.append(line.split('\t')
   if len(split_line) != 39:
     print('line error!! not right number of tabs', len(split_line), split_line[8])
 
@@ -25,7 +27,10 @@ for line in lines_merged:
 #in some instances there are an unexpected '\t' in front of phone number
 #this is impossible to anticipate
     
-    
+print('total lines after mid line change line sign removal:', len(lines_merged))
+
+
+
 inp.close()
 oup.close()
 
