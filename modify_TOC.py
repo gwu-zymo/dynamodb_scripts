69fd4a8e5376307f45e20f4f82e695545a4a67b0
gwu-zymo <47827599+gwu-zymo@users.noreply.github.com>
2023-04-11 12:11:41 -0700

    Update modify_TOC.py

--- modify_TOC.py
+++ modify_TOC.py
@@ -4,8 +4,9 @@ float_list = ["ct_V1V3", "ct_ITS", "ct_AMR1", "ct_AMR2", "ct_AMRm", "Age_Years"]
 
 inp = open('TOC_4_10_23.txt', 'r')
 oup = open('TOC_4_10_23_mod.txt', 'w')
-header = inp.readline()
+line = inp.readline()
 oup.write(line)
+header = line.strip('\n').split('\t')
 lines = inp.readlines()
 lines_merged = []
 tmp_line = ''
@@ -19,19 +20,40 @@ for i in range(len(lines)-1, -1, -1):
 lines_merged_split = []
 for line in lines_merged:
   split_line = line.split('\t')
-  lines_merged_split.append(line.split('\t')
+  lines_merged_split.append(line.split('\t'))
   if len(split_line) != 39:
     print('line error!! not right number of tabs', len(split_line), split_line[8])
+
+print('total lines after mid line change line sign removal:', len(lines_merged))
 
 #manually fix the errors and then continue
 #in some instances there are an unexpected '\t' in front of phone number
 #this is impossible to anticipate
-    
-print('total lines after mid line change line sign removal:', len(lines_merged))
+                            
+float_i = []
+for i in range(0, len(header)):
+  if header[i] in float_list:
+    float_i.append(i)
 
-
+lines_merged_split_m = []
+for sample in lines_merged_split:
+  for i in range(0, len(sample)):
+    if i in float_i:
+      if sample[i] != '':
+        try:
+          tmp = float(sample[i])
+        except:
+          sample[i] = ''
+  lines_merged_split_m.append(sample)
+  oup.write('\t'.join(sample) + '\n')
 
 inp.close()
 oup.close()
 
-  
+
+                            
+                            
+                            
+                            
+                            
+                      
